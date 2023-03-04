c5eba30c831957c8b07c01f4cdb85342345d7be8
Andrew Kane <andrew@ankane.org>
2023-03-04 10:46:14 -0800

    Added parse method

--- LibPQ/example.jl
+++ LibPQ/example.jl
@@ -8,6 +8,8 @@ execute(conn, "CREATE TABLE items (embedding vector(3))")
 
 module Pgvector
     convert(v::AbstractVector{T}) where T<:Real = string("[", join(v, ","), "]")
+
+    parse(v::String) = map(x -> Base.parse(Float32, x), split(v[2:end-1], ","))
 end
 
 embeddings = [1 1 1; 2 2 2; 1 1 2]
@@ -20,7 +22,7 @@ LibPQ.load!(
 embedding = Pgvector.convert([1, 1, 1])
 result = execute(conn, "SELECT * FROM items ORDER BY embedding <-> \$1 LIMIT 5", [embedding])
 data = columntable(result)
-println(data)
+println(map(Pgvector.parse, data.embedding))
 
 execute(conn, "CREATE INDEX my_index ON items USING ivfflat (embedding vector_l2_ops)")
 
