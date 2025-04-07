02e86792fc8bc50eaa61ed138f2488188e635aa6
Andrew Kane <andrew@ankane.org>
2025-04-07 02:16:16 -0700

    Updated example [skip ci]

--- examples/cohere/example.jl
+++ examples/cohere/example.jl
@@ -4,11 +4,7 @@ conn = LibPQ.Connection("dbname=pgvector_example host=localhost")
 
 execute(conn, "CREATE EXTENSION IF NOT EXISTS vector")
 execute(conn, "DROP TABLE IF EXISTS documents")
-execute(conn, "CREATE TABLE documents (id bigserial PRIMARY KEY, content text, embedding vector(1024))")
-
-module Pgvector
-    convert(v::AbstractVector{T}) where T<:Real = string("[", join(v, ","), "]")
-end
+execute(conn, "CREATE TABLE documents (id bigserial PRIMARY KEY, content text, embedding bit(1024))")
 
 function embed(texts, type)
     url = "https://api.cohere.com/v1/embed"
@@ -16,14 +12,15 @@ function embed(texts, type)
         "texts" => texts,
         "model" => "embed-english-v3.0",
         "input_type" => type,
-        "embedding_types" => ["float"]
+        "embedding_types" => ["ubinary"]
     )
     headers = [
         "authorization" => string("Bearer ", ENV["CO_API_KEY"]),
         "content-type" => "application/json"
     ]
     r = HTTP.request("POST", url, headers, JSON.json(data))
-    [map(identity, v) for v in JSON.parse(String(r.body))["embeddings"]["float"]]
+    ubinary = JSON.parse(String(r.body))["embeddings"]["ubinary"]
+    [join(map(v -> bitstring(UInt8(v)), u)) for u in ubinary]
 end
 
 input = [
@@ -33,14 +30,14 @@ input = [
 ]
 embeddings = embed(input, "search_document")
 LibPQ.load!(
-    (content = input, embedding = map(Pgvector.convert, embeddings),),
+    (content = input, embedding = embeddings),
     conn,
     "INSERT INTO documents (content, embedding) VALUES (\$1, \$2)",
 )
 
 query = "forest"
 embedding = embed([query], "search_query")[1]
-result = execute(conn, "SELECT id FROM documents ORDER BY embedding <=> \$1 LIMIT 5", [Pgvector.convert(embedding)])
+result = execute(conn, "SELECT id FROM documents ORDER BY embedding <~> \$1 LIMIT 5", [embedding])
 rows = Tables.rows(columntable(result))
 for row in rows
     println(Tables.getcolumn(row, 1))
